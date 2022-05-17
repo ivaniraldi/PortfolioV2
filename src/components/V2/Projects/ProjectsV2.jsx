c791e742dda5db126bee45fade9fb92a73fedfa2
ivaniraldi <iraldiban@gmail.com>
2022-05-17 17:36:36 -0300

    fixed deploy

--- src/components/V2/Projects/ProjectsV2.jsx
+++ src/components/V2/Projects/ProjectsV2.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { motion } from 'framer-motion'
-import ParticlesStars from '../../particles/particlesStars'
-import ParticlesBackground from '../../particles/particleBackground'
 
 
 export default function ProjectsV2() {
